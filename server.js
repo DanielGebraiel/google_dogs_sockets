93df91979f69ecb98b921f733359ba45d6f7301d
Daniel Gebraiel <daniel.gebraiel01@eng-st.cu.edu.eg>
2024-05-14 06:03:59 +0300

    added rooms for different docs

--- server.js
+++ server.js
@@ -31,23 +31,33 @@ pool.query("SELECT NOW()", (err, res) => {
 io.on("connection", (socket) => {
   console.log("A user connected");
 
+  socket.on("join", (docId) => {
+    socket.join(docId);
+    console.log(`User joined room: ${docId}`);
+  });
+
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
 
-  socket.on("insert", (char, uniqueId, fractionalId, isBold, isItalic) => {
-    console.log(char);
-    console.log(uniqueId);
-    console.log(fractionalId);
-    console.log(isBold);
-    console.log(isItalic);
-    crdt.insert(char, uniqueId, fractionalId, isBold, isItalic); // Use CRDT to handle insertions
-    io.emit("insert", char, uniqueId, fractionalId, isBold, isItalic); // Broadcast insertions to clients
-  });
+  socket.on(
+    "insert",
+    (docId, char, uniqueId, fractionalId, isBold, isItalic) => {
+      console.log(char);
+      console.log(uniqueId);
+      console.log(fractionalId);
+      console.log(isBold);
+      console.log(isItalic);
+      crdt.insert(char, uniqueId, fractionalId, isBold, isItalic);
+      socket
+        .to(docId)
+        .emit("insert", char, uniqueId, fractionalId, isBold, isItalic); // Broadcast insertions to clients in the same room
+    }
+  );
 
-  socket.on("delete", (uniqueId, fractionalId) => {
+  socket.on("delete", (docId, uniqueId, fractionalId) => {
     crdt.delete(uniqueId, fractionalId); // Use CRDT to handle deletions
-    io.emit("delete", uniqueId, fractionalId); // Broadcast deletions to clients
+    socket.to(docId).emit("delete", uniqueId, fractionalId); // Broadcast deletions to clients in the same room
   });
 });
 
